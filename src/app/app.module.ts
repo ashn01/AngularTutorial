121b2334ca6a37e9bf95b2cad8b64233146e8456
YKStudio <ashn01@naver.com>
2020-05-09 15:48:18 -0400

    first tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
+import { TimeDisplayComponent } from './section/time-display/time-display.component';
+import { ButtonComponent } from './section/button/button.component';
 
+// decorator
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SectionComponent,
+    TimeDisplayComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
